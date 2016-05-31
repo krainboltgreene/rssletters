2b6b320dbd5521ead3682261f976f9c42235527d
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-05-31 12:43:17 -0700

    Turns out it's an array

--- app/former/newsletter_former.rb
+++ app/former/newsletter_former.rb
@@ -1,6 +1,6 @@
 class NewsletterFormer
   def initialize(parameters)
-    @address = Address.find_by!(id: id(parameters["ToFull"]["Email"]))
+    @address = Address.find_by!(id: id(parameters["ToFull"][0]["Email"]))
     @provider = @address.provider
 
     @newsletter = Newsletter.new(
